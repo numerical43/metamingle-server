843636fbdf732ef5f96555c29c3fafe0166bdbb1
root <root@LAPTOP-O91Q221D>
2023-10-15 17:42:44 +0900

    프로젝트 설정

---
+++ src/main/java/com/mingles/metamingle/MetaMingleApplication.java
@@ -0,0 +1,13 @@
+package com.mingles.metamingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaMingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaMingleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mingles/metamingle/MetaMingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingles.metamingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaMingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
