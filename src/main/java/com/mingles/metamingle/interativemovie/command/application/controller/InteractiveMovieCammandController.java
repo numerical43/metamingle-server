11300762db6ea7639c29490f94536b9310120cad
sui kang <numerical43@daum.net>
2023-10-18 15:30:59 +0900

    feature : (#9) interactiveMovie Command 기본 클래스 생성

---
+++ src/main/java/com/mingles/metamingle/interativemovie/command/application/controller/InteractiveMovieCammandController.java
@@ -0,0 +1,10 @@
+package com.mingles.metamingle.interativemovie.command.application.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class InteractiveMovieCammandController {
+
+}
---
+++ src/main/java/com/mingles/metamingle/interativemovie/command/application/service/InteractiveMovieCommandService.java
@@ -0,0 +1,9 @@
+package com.mingles.metamingle.interativemovie.command.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class InteractiveMovieCommandService {
+}
