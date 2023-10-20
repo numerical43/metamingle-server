6472599e79228aea9a4b5153dc0ed0be1e415e59
sui kang <numerical43@daum.net>
2023-10-20 18:39:03 +0900

    feature : (#17) data.sql 작성

---
+++ src/main/resources/db.data/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO tbl_short_form(short_form_no, date, description, is_interactive, member_no, thumbnail_url, title, url)
+VALUES (1, '2023-10-20T04:43:29.783+00:00', '영상 설명', FALSE, 1,
+        'https://firebasestorage.googleapis.com/v0/b/meta-mingle.appspot.com/o/thumbnails%2F4d47053e-5eaf-4f7c-8360-93df0e4ff6e220231020134326.jpeg?alt=media', '제목',
+        'https://firebasestorage.googleapis.com/v0/b/meta-mingle.appspot.com/o/4d47053e-5eaf-4f7c-8360-93df0e4ff6e220231020134326.mp4?alt=media');
+
+INSERT INTO tbl_short_form(short_form_no, date, description, is_interactive, member_no, thumbnail_url, title, url)
+VALUES (2, '2023-10-20T04:43:29.783+00:00', '영상 설명', FALSE, 1,
+        'https://firebasestorage.googleapis.com/v0/b/meta-mingle.appspot.com/o/thumbnails%2F5b8acaa2-0cc5-4826-83c6-e23ec181d8e020231020133238.jpeg?alt=media', '제목',
+        'https://firebasestorage.googleapis.com/v0/b/meta-mingle.appspot.com/o/5b8acaa2-0cc5-4826-83c6-e23ec181d8e020231020133238.mp4?alt=media');
+
+
+INSERT INTO tbl_member(member_no, member_name)
+VALUES (1, '닉네임');
