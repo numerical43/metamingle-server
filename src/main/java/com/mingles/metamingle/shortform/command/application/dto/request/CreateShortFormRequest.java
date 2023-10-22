dbf3ef5694f7d5e1788c19f1e8678102b102e972
sui kang <numerical43@daum.net>
2023-10-22 15:35:51 +0900

    delete : (#17) 필요없는 파일 삭제

--- src/main/java/com/mingles/metamingle/shortform/command/application/dto/request/CreateShortFormRequest.java
+++
@@ -1,9 +0,0 @@
-package com.mingles.metamingle.shortform.command.application.dto.request;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class CreateShortFormRequest {
-}
