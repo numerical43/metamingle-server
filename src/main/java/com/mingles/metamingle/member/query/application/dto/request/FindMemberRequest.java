6d4d8a3efb3df3b1e8e71a046d6d16dea467af9f
Son Jung In <jungin.dylan@gmail.com>
2023-10-22 15:48:20 +0900

    feat: member query req, res

---
+++ src/main/java/com/mingles/metamingle/member/query/application/dto/request/FindMemberRequest.java
@@ -0,0 +1,14 @@
+package com.mingles.metamingle.member.query.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class FindMemberRequest {
+
+    private Long memberNo;
+
+}
