7b4b21dc0586c4b496e63e58f12f38a24fb6e2d1
Son Jung In <jungin.dylan@gmail.com>
2023-10-23 15:33:59 +0900

    fix: response output으로 수정

--- src/main/java/com/mingles/metamingle/script/command/application/dto/response/ScriptInfraResponse.java
+++ src/main/java/com/mingles/metamingle/script/command/application/dto/response/ScriptInfraResponse.java
@@ -2,11 +2,15 @@ package com.mingles.metamingle.script.command.application.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ScriptInfraResponse{
 
-    private String text;
+    private String output;
 
 }
