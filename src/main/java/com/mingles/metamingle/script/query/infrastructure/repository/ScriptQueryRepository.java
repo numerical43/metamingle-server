3016e91b6ec4c0cdfaac7bca8f7efcb36131790b
Son Jung In <jungin.dylan@gmail.com>
2023-10-23 16:33:07 +0900

    feat: #31

--- src/main/java/com/mingles/metamingle/script/query/infrastructure/repository/ScriptQueryRepository.java
+++ src/main/java/com/mingles/metamingle/script/query/infrastructure/repository/ScriptQueryRepository.java
@@ -2,9 +2,15 @@
 package com.mingles.metamingle.script.query.infrastructure.repository;
 
 import com.mingles.metamingle.script.command.domain.aggregate.entity.Script;
+import com.mingles.metamingle.script.command.domain.aggregate.vo.ShortFormNoVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ScriptQueryRepository extends JpaRepository<Script, Long> {
+
+    Optional<Script> findScriptByShortFormNoVO(ShortFormNoVO shortFormNoVO);
+
 }
