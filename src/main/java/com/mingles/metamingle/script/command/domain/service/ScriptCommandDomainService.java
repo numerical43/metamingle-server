53923220ab9dcc3ecec019cbea22fcdc8c09033d
Son Jung In <jungin.dylan@gmail.com>
2023-10-23 09:37:57 +0900

    fix: 언어/길이 검증 분리

--- src/main/java/com/mingles/metamingle/script/command/domain/service/ScriptCommandDomainService.java
+++ src/main/java/com/mingles/metamingle/script/command/domain/service/ScriptCommandDomainService.java
@@ -8,14 +8,22 @@ import java.util.regex.Pattern;
 @Service
 public class ScriptCommandDomainService {
 
-    public void validateScriptContent(String scriptContent) {
+    public boolean validateScriptLanguage(String scriptContent) {
 
         boolean isKorean = Pattern.matches("^[ㄱ-ㅎ가-힣]*$", scriptContent);
 //        boolean isKoreanOrEnglish = Pattern.matches("^[ㄱ-ㅎ가-힣a-zA-Z]*$", scriptContent);
+
+        System.out.println("isKorean = " + isKorean);
+
+        return isKorean;
+
+    }
+
+    public void validateScriptLength(String scriptContent) {
+
         int length = scriptContent.getBytes(Charset.defaultCharset()).length;
 
         System.out.println("length = " + length);
-        System.out.println("isKorean = " + isKorean);
 
     }
 
--- src/main/java/com/mingles/metamingle/script/command/infrastructure/service/ScriptCommandInfraService.java
+++ src/main/java/com/mingles/metamingle/script/command/infrastructure/service/ScriptCommandInfraService.java
@@ -25,6 +25,7 @@ public class ScriptCommandInfraService {
 //                .retrieve()
 //                .bodyToMono(ScriptInfraResponse.class)
 //                .block();
+//        System.out.println("aiScript = " + aiScript.toString());
 //
 //        return aiScript;
 
@@ -35,7 +36,7 @@ public class ScriptCommandInfraService {
                 "(생각하는 표정으로) 어제, 누군가 나에게 아주 이상한 말을 던졌어: \"너 발이 넓다\". 발이 작은 나는 이 말을 이해하지 못했지!  그치만, 알고보니 그게 단순히 친구가 많다는 뜻이라니!\n" +
                 "(놀라는 표정으로) 이런 아주 신기한 속담이 있었다니! 나, 평생 동안 상상도 못했었어.\n" +
                 "결론:\n" +
-                "(웃으며) 이제부터 나는 \"발이 넓다\"고 했을 때, 그게 내가 많은 사람들과 괜찮은 관계를 갖고 있다는 속담의 하나라는 걸 않겠 됬 거야. " +
+                "(웃으며) 이제부터 나는 \"발이 넓다\"고 했을 때, 그게 내가 많은 사람들과 괜찮은 관계를 갖고 있다는 속담의 하나라는 걸 알게 된 거야. " +
                 "일상 속에서 흔히 사용되는 말들 중에 어떤 깊은 뜻이 숨어있는지 모르니까, 새로운 것을 알게 되는건 정말 재밌어. 공부 하는건 정말 재밌있는 일이야!");
 
     }
