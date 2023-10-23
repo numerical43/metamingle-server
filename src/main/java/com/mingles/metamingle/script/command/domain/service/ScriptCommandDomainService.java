c6f4fb2a7f3e75f9844003dfcc991933c33f5510
Son Jung In <jungin.dylan@gmail.com>
2023-10-23 09:43:09 +0900

    feat: #16

--- src/main/java/com/mingles/metamingle/script/command/domain/service/ScriptCommandDomainService.java
+++ src/main/java/com/mingles/metamingle/script/command/domain/service/ScriptCommandDomainService.java
@@ -8,14 +8,16 @@ import java.util.regex.Pattern;
 @Service
 public class ScriptCommandDomainService {
 
-    public boolean validateScriptLanguage(String scriptContent) {
+    public void validateScriptLanguage(String scriptContent) {
 
         boolean isKorean = Pattern.matches("^[ㄱ-ㅎ가-힣]*$", scriptContent);
 //        boolean isKoreanOrEnglish = Pattern.matches("^[ㄱ-ㅎ가-힣a-zA-Z]*$", scriptContent);
 
         System.out.println("isKorean = " + isKorean);
 
-        return isKorean;
+        if(!isKorean) {
+            throw new IllegalArgumentException("한국어가 아닌 언어는 사용이 불가능합니다.");
+        }
 
     }
 
@@ -24,6 +26,10 @@ public class ScriptCommandDomainService {
         int length = scriptContent.getBytes(Charset.defaultCharset()).length;
 
         System.out.println("length = " + length);
+
+        if(length > 1000) {
+            throw new IllegalArgumentException("글자수는 1000자를 넘길 수 없습니다.");
+        }
 
     }
 
