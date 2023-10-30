9ccdda939f14c862ad486c72aae6f614aa2fa5b0
sui kang <numerical43@daum.net>
2023-10-31 00:03:23 +0900

    feature : #50 InteractiveMovieLike optional로 변경

--- src/main/java/com/mingles/metamingle/movielike/query/domain/repository/InteractiveMovieLikeQueryRepository.java
+++ src/main/java/com/mingles/metamingle/movielike/query/domain/repository/InteractiveMovieLikeQueryRepository.java
@@ -5,10 +5,12 @@ import com.mingles.metamingle.movielike.command.domain.aggregate.vo.InteractiveM
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InteractiveMovieLikeQueryRepository extends JpaRepository<InteractiveMovieLike, InteractiveMovieLikeVO> {
 
     int countAllByInteractiveMovieLikeVO_InteractiveMovieNo(Long interactiveMovieNo);
 
-    InteractiveMovieLike findInteractiveMovieLikeByInteractiveMovieLikeVO(InteractiveMovieLikeVO interactiveMovieLikeVO);
+    Optional<InteractiveMovieLike> findInteractiveMovieLikeByInteractiveMovieLikeVO(InteractiveMovieLikeVO interactiveMovieLikeVO);
 }
