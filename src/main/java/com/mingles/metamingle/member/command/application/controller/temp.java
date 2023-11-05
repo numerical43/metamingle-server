ae1b93b19f87265aeebc86942830d52450ecdc06
SonJungin <jungin.dylan@gmail.com>
2023-11-06 08:53:42 +0900

    temp 삭제

--- src/main/java/com/mingles/metamingle/member/command/application/controller/temp.java
+++
@@ -1,4 +0,0 @@
-package com.mingles.metamingle.member.command.application.controller;
-
-public class temp {
-}
--- src/main/java/com/mingles/metamingle/member/command/application/dto/response/temp.java
+++
@@ -1,4 +0,0 @@
-package com.mingles.metamingle.member.command.application.dto.response;
-
-public class temp {
-}
