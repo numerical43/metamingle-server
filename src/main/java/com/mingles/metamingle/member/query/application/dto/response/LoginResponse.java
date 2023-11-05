f42e3a0f9575676a4273ca8a4c1c1cf709ce09ae
SonJungin <jungin.dylan@gmail.com>
2023-11-06 08:59:15 +0900

    feat: #67

---
+++ src/main/java/com/mingles/metamingle/member/query/application/dto/response/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.mingles.metamingle.member.query.application.dto.response;
+
+
+import com.mingles.metamingle.member.command.domain.aggregate.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+
+}
