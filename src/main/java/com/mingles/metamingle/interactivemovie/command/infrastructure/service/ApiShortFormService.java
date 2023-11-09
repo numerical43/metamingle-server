e9ecc4985d52a078060701dd6144e19e78c4ffbc
sui kang <numerical43@daum.net>
2023-11-09 15:03:40 +0900

    feature : #80 토큰으로 사용자 구별하도록 수정

--- src/main/java/com/mingles/metamingle/interactivemovie/command/infrastructure/service/ApiShortFormService.java
+++ src/main/java/com/mingles/metamingle/interactivemovie/command/infrastructure/service/ApiShortFormService.java
@@ -1,10 +1,12 @@
 package com.mingles.metamingle.interactivemovie.command.infrastructure.service;
 
+import com.mingles.metamingle.auth.JwtTokenProvider;
 import com.mingles.metamingle.shortform.command.application.service.ShortFormFirebaseService;
 import com.mingles.metamingle.shortform.command.domain.aggregate.entity.ShortForm;
 import lombok.RequiredArgsConstructor;
 import org.jcodec.api.JCodecException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -14,11 +16,14 @@ import java.io.IOException;
 public class ApiShortFormService {
 
     private final ShortFormFirebaseService shortFormFirebaseService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public ShortForm createShortFormWithInteractiveMovie(MultipartFile file, String title, String description)
+    public ShortForm createShortFormWithInteractiveMovie(@RequestHeader("Authorization") String token, MultipartFile file, String title, String description)
             throws JCodecException, IOException {
 
-        return shortFormFirebaseService.createShortFormWithInteractiveMovie(file, title, description);
+        Long memberNo = jwtTokenProvider.getMemberNoFromToken(token);
+
+        return shortFormFirebaseService.createShortFormWithInteractiveMovie(file, title, description, memberNo);
 
     }
 }
--- src/main/java/com/mingles/metamingle/shortform/query/application/controller/ShortFormQueryController.java
+++ src/main/java/com/mingles/metamingle/shortform/query/application/controller/ShortFormQueryController.java
@@ -1,5 +1,6 @@
 package com.mingles.metamingle.shortform.query.application.controller;
 
+import com.mingles.metamingle.auth.JwtTokenProvider;
 import com.mingles.metamingle.common.ApiResponse;
 import com.mingles.metamingle.shortform.query.application.dto.response.GetShortFormListResponse;
 import com.mingles.metamingle.shortform.query.application.dto.response.GetShortFormResponse;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,10 +19,13 @@ import java.util.List;
 public class ShortFormQueryController {
 
     private final ShortFormQueryService shortFormQueryService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     // 숏폼 리스트 조회
     @GetMapping("/short-form")
-    public ResponseEntity<ApiResponse> getShortFormList() {
+    public ResponseEntity<ApiResponse> getShortFormList(@RequestHeader("Authorization") String token) {
+
+        Long memberNo = jwtTokenProvider.getMemberNoFromToken(token);
 
         List<GetShortFormListResponse> response = shortFormQueryService.getShortFormList();
 
@@ -29,7 +34,9 @@ public class ShortFormQueryController {
 
     // 숏폼 하나 상세 조회
     @GetMapping("/short-form/{shortFormNo}")
-    public ResponseEntity<ApiResponse> getShortForm(@PathVariable("shortFormNo") Long shortFormNo) {
+    public ResponseEntity<ApiResponse> getShortForm(@RequestHeader("Authorization") String token, @PathVariable("shortFormNo") Long shortFormNo) {
+
+        Long memberNo = jwtTokenProvider.getMemberNoFromToken(token);
 
         GetShortFormResponse response = shortFormQueryService.getShortForm(shortFormNo);
 
