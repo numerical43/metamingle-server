85801bbd6b89a2bb319c53b794168cb8f3b75177
SonJungin <jungin.dylan@gmail.com>
2023-11-15 07:31:48 +0900

    feat: #74

--- src/main/java/com/mingles/metamingle/creativetool/query/domain/mongo/SoundRepository.java
+++ src/main/java/com/mingles/metamingle/creativetool/query/domain/mongo/SoundRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface SoundRepository extends MongoRepository<Sound, String> {
-    List<Sound> findSoundsByMood(String mood);
+    List<Sound> findSoundsByMood(String mood );
 }
