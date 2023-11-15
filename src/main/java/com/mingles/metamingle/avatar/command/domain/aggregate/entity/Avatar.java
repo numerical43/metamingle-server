99c090a813511159fb429727cc4c39ed2b5e7926
SonJungin <jungin.dylan@gmail.com>
2023-11-15 21:23:01 +0900

    fix: 아바타 엔티티 수정

--- src/main/java/com/mingles/metamingle/avatar/command/domain/aggregate/entity/Avatar.java
+++ src/main/java/com/mingles/metamingle/avatar/command/domain/aggregate/entity/Avatar.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -18,65 +19,13 @@ public class Avatar implements Serializable {
     @EmbeddedId
     private AvatarMemberNoVO avatarMemberNoVO;
 
-    @Column
-    private String skinColor;
-
-    @Column
-    private String hair;
-
-    @Column
-    private String hairColor;
-
-    @Column
-    private String eye;
-
-    @Column
-    private String eyeColor;
-
-    @Column
-    private String top;
-
-    @Column
-    private String topColor;
-
-    @Column
-    private String bottom;
-
-    @Column
-    private String bottomColor;
-
-    @Column
-    private String onePiece;
-
-    @Column
-    private String onPieceColor;
-
-    @Column
-    private String shoes;
-
-    @Column
-    private String ShoeColor;
-
-    @Column
-    private String gender;
+    @Size(max = 3000)
+    private String avatarData;
 
     @Builder
-    public Avatar(AvatarMemberNoVO avatarMemberNoVO, String skinColor, String hair, String hairColor, String eye, String eyeColor, String top, String topColor, String bottom, String bottomColor, String onePiece, String onPieceColor, String shoes, String shoeColor, String gender) {
+    public Avatar(AvatarMemberNoVO avatarMemberNoVO, String avatarData) {
         this.avatarMemberNoVO = avatarMemberNoVO;
-        this.skinColor = skinColor;
-        this.hair = hair;
-        this.hairColor = hairColor;
-        this.eye = eye;
-        this.eyeColor = eyeColor;
-        this.top = top;
-        this.topColor = topColor;
-        this.bottom = bottom;
-        this.bottomColor = bottomColor;
-        this.onePiece = onePiece;
-        this.onPieceColor = onPieceColor;
-        this.shoes = shoes;
-        ShoeColor = shoeColor;
-        this.gender = gender;
+        this.avatarData = avatarData;
     }
 
 }
