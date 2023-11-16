147ee27d320514bb66b9947afbf1b2386ad7a43b
sui <numerical43@daum.net>
2023-11-16 16:38:50 +0900

    feature : #103 퀴즈 Entity, 기본 클래스 생성

--- src/main/java/com/mingles/metamingle/quiz/query/domain/repository/QuizQueryRepository.java
+++ src/main/java/com/mingles/metamingle/quiz/query/domain/repository/QuizQueryRepository.java
@@ -1,6 +1,7 @@
 package com.mingles.metamingle.quiz.query.domain.repository;
 
 import com.mingles.metamingle.quiz.command.domain.aggregate.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
