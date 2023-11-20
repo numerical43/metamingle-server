b3e2f8975c03b31de9c1f2b6020323b2c14ab246
SonJungin <jungin.dylan@gmail.com>
2023-11-21 01:20:06 +0900

    feat: quiz rank controller, service

---
+++ src/main/java/com/mingles/metamingle/quiz/query/application/dto/response/QuizRankQueryResponse.java
@@ -0,0 +1,21 @@
+package com.mingles.metamingle.quiz.query.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuizRankQueryResponse {
+
+    private int rankNo;
+    private String english;
+    private String korean;
+    private Long shortFormNo;
+
+}
