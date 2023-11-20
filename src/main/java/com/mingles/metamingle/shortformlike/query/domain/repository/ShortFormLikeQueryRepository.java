399f5da15e782b820cb09dd2127b88e2a83d16d4
SonJungin <jungin.dylan@gmail.com>
2023-11-21 01:20:25 +0900

    temp

--- src/main/java/com/mingles/metamingle/shortformlike/query/domain/repository/ShortFormLikeQueryRepository.java
+++ src/main/java/com/mingles/metamingle/shortformlike/query/domain/repository/ShortFormLikeQueryRepository.java
@@ -4,8 +4,10 @@ package com.mingles.metamingle.shortformlike.query.domain.repository;
 import com.mingles.metamingle.shortformlike.command.domain.aggregate.entity.ShortFormLike;
 import com.mingles.metamingle.shortformlike.command.domain.aggregate.vo.ShortFormLikeVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,9 @@ public interface ShortFormLikeQueryRepository extends JpaRepository<ShortFormLik
 
     Optional<ShortFormLike> findShortFormLikeByShortFormLikeVO(ShortFormLikeVO shortFormLikeVO);
 
+    @Query("SELECT sflike.shortFormLikeVO.shortFormNo, COUNT(sflike) as likeCount " +
+            "FROM ShortFormLike sflike " +
+            "GROUP BY sflike.shortFormLikeVO.shortFormNo " +
+            "ORDER BY likeCount DESC ")
+    List<Object[]> findTop12LikedShortForms();
 }
