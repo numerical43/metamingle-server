97d19b6b87f9b2e3ab22a24dc0d60d69d19f87a1
SonJungin <jungin.dylan@gmail.com>
2023-11-20 16:30:06 +0900

    temp 삭제

--- src/main/java/com/mingles/metamingle/quiz/command/infrastructure/service/QuizInfraCommandService.java
+++
@@ -1,9 +0,0 @@
-package com.mingles.metamingle.quiz.command.infrastructure.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class QuizInfraCommandService {
-}
